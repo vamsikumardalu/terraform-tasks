6e96ea10e80ffb387627b31e6a55bac105cb5946
prudhvisurya996 <150457002+prudhvisurya996@users.noreply.github.com>
2024-03-12 20:57:03 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "us-east-2"
-  access_key = "AKIAZWWURU2IEWSH7TV"
-  secret_key = "VL/Wl/CD7Cp8Ttx90VhsjVh0/ylexuYV7oIRw7i"
+  access_key = "AKIAZWWURU2IEWSH7TVE"
+  secret_key = "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"
 }
 
