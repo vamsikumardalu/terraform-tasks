09573e52dbb8d9bccb466c01b4490de2ccbd55c8
prudhvisurya996 <150457002+prudhvisurya996@users.noreply.github.com>
2024-03-12 20:26:54 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,1 +1,10 @@
-
+resource "aws_instance" "dove" {
+  ami                    = "ami-02ca28e7c7b8f8be1"
+  instance_type          = "t2.micro"
+  availability_zone      = "us-east-2a"
+  key_name               = "dove-key"
+  vpc_security_group_ids = ["sg-0684742b534c2aa40"]
+  tags = {
+    Name = "dove-instance"
+  }
+}
