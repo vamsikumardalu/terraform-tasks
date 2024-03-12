4e14b74d52128dbc2febc0fc8d916480bd27b8e0
prudhvisurya996 <150457002+prudhvisurya996@users.noreply.github.com>
2024-03-12 20:42:41 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "dove" {
-  ami                    = "ami-02ca28e7c7b8f8be1"
+  ami                    = "ami-022661f8a4a1b91cf"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-2a"
   key_name               = "dove-key"
