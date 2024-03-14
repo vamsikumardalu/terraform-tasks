de3006f8511f9d1ff4797ca8f1ef18bffe8a9626
vamsikumardalu <143592301+vamsikumardalu@users.noreply.github.com>
2024-03-14 20:35:07 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,10 +1,8 @@
 resource "aws_instance" "dove" {
-  ami                    = "ami-022661f8a4a1b91cf"
+  ami                    = "ami-08f7912c15ca96832"
   instance_type          = "t2.micro"
-  availability_zone      = "us-east-2a"
-  key_name               = "dove-key"
-  vpc_security_group_ids = ["sg-0684742b534c2aa40"]
+  key_name               = "vamsi22"
   tags = {
-    Name = "dove-instance"
+    Name = "vamsi"
   }
 }
